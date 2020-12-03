737b8b7b43faf0e93a21568646bffaef1a1b4848
Shanko <scubashanko@gmail.com>
2020-12-02 20:07:40 -0500

    Add demo react dashboard frontend for visuals

---
+++ frontend/gatsby-theme/src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { Link } from 'gatsby-theme-material-ui';
+import ProTip from '../components/ProTip';
+import Copyright from '../components/Copyright';
+import Dashboard from '../components/Dashboard';
+
+export default function Index() {
+  return (
+    <Dashboard />
+  );
+}
